2ecfc7584cf3bc82149e112cdd362d8b45253ed7
“Sanjaya <sanjaya121@gmail.com>
2025-03-07 04:08:30 -0600

    form validation

--- angular-client-ui/src/app/components/auth/sign-up/sign-up.component.ts
+++ angular-client-ui/src/app/components/auth/sign-up/sign-up.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Form, FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Form, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -15,18 +15,39 @@ export class SignUpComponent implements OnInit {
    */
 
   singUpForm: FormGroup // form initialization.
+  formData = {};
   constructor() {
 
     this.singUpForm = new FormGroup({
-      email: new FormControl(''),
-      firstName: new FormControl(''),
-      lastName: new FormControl(''),
-      password: new FormControl('')
+      email: new FormControl('', [Validators.required, Validators.email]),
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+      confirmPassword: new FormControl('', Validators.required)
 
+    })
+    this.singUpForm.valueChanges.subscribe(values => {
+      this.formData = values;
+      console.log("form Data", this.singUpForm);
     })
   }
 
   ngOnInit(): void {
-      
+    console.log("ngOnInit", this.singUpForm);
+  }
+  get email() {
+    return this.singUpForm.controls['email'];
+  }
+  get firstName() {
+    return this.singUpForm.controls['firstName'];
+  }
+  get lastName() {
+    return this.singUpForm.controls['lastName'];
+  }
+  get password() {
+    return this.singUpForm.controls['password'];
+  }
+  get confirmPassword() {
+    return this.singUpForm.controls['confirmPassword'];
   }
 }
