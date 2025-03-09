977fc67973c6d71d06f00f38b784f9531e84086c
“Sanjaya <sanjaya121@gmail.com>
2025-03-09 03:09:28 -0500

    login form

--- angular-client-ui/src/app/components/routes/login/login.component.ts
+++ angular-client-ui/src/app/components/routes/login/login.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule,ReactiveFormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+login() {
+throw new Error('Method not implemented.');
+}
 
+
+  loginForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: this.formBuilder.control('',[Validators.required,Validators.email]),
+      password: this.formBuilder.control('',Validators.required)
+    });
+
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+  get password() {
+    return this.loginForm.get('password');
+  }
 }
